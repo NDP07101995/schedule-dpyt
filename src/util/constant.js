6363e9781af29c9d9caa8c31d6cd12545e1f7f19
NDPhuong1995 <nguyendatphuong0710@gmail.com>
2019-05-23 19:14:48 +0700

    commit new version

---
+++ src/util/constant.js
@@ -0,0 +1,4 @@
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/';
+export const SEARCH_YOUTUBE_API_URL = YOUTUBE_API_URL + 'search';
+export const PLAYLIST_YOUTUBE_API_URL = YOUTUBE_API_URL + 'playlists';
+export const PLAYLIST_YOUTUBE_API_URL = YOUTUBE_API_URL + 'playlistItems';
